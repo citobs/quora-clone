69bb3a42d94c52d64bb5c3dbca6f611ef819fc80
citobs <altiorapetamus7@hanmail.net>
2023-01-23 17:28:28 +0900

    로그인 기능 구현/및 파베연동 성공적

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 import Quora from './Quora';
 import Navbar from './Navbar';
+import { useSelect } from '@mui/base';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="App">
-      
+      {
+        user ? (<Quora/>) : (<Login/>)
+      }
       <Quora/>
    
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBIKJzIK6mBH_zu4ILRkwGdYSBtoGQBWT4",
